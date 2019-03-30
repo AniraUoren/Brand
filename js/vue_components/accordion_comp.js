c97703b54d2a76e6a3b7cecaa8cd6dcbce1aba80
AniraUoren <Airin6666@yandex.ru>
2019-03-30 16:41:58 +0300

    end accordion_comp

--- js/vue_components/accordion_comp.js
+++ js/vue_components/accordion_comp.js
@@ -5,45 +5,35 @@ Vue.component("accordion", {
             category: [],
             brand: [],
             designer: [],
+            isShowedCategory: true,
+            isShowedBrand: false,
+            isShowedDesigner: false,
+            up: "fa-caret-up",
+            down: "fa-caret-down",
             menuURL: "/menu.json"
         }
     },
     template:`
         <div class="shop-menu">
             <ul class="shop-menu-main-list">
-                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link">Category<i class="fa fa-caret-up shop-menu-main-element-caret" aria-hidden="true"></i></a></li>
+                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link" :class="{unactive : !isShowedCategory}" @click.prevent="isShowedCategory = !isShowedCategory">Category<i class="fa" :class="[isShowedCategory ? up : down]" aria-hidden="true"></i></a></li>
                 <ul class="shop-submenu-list">
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Accessories</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Bags</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Denim</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Hoodies & Sweatshirts</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Jackets & Coats</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Pants</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Polos</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Shirts</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Shoes</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Shorts</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Sweaters & Knits</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">T-Shirts</a></li>
-                    <li class="shop-submenu-elements"><a href="#" class="shop-submenu-link">Tanks</a></li>
+                    <li class="shop-submenu-elements" v-for="item of category" v-show="isShowedCategory"><a href="#" class="shop-submenu-link">{{ item }}</a></li>
                 </ul>
-                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link unactive">BRAND<i class="fa fa-caret-down" aria-hidden="true"></i></a></li>
-                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link unactive">dESIGNER<i class="fa fa-caret-down" aria-hidden="true"></i></a></li>
+                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link" :class="{unactive : !isShowedBrand}" @click.prevent="isShowedBrand = !isShowedBrand">BRAND<i class="fa" :class="[isShowedBrand ? up : down]" aria-hidden="true"></i></a></li>
+                <ul class="shop-submenu-list">
+                    <li class="shop-submenu-elements" v-for="item of brand" v-show="isShowedBrand"><a href="#" class="shop-submenu-link">{{ item }}</a></li>
+                </ul>
+                <li class="shop-menu-main-element"><a href="#" class="shop-menu-main-link" :class="{unactive : !isShowedDesigner}" @click.prevent="isShowedDesigner = !isShowedDesigner">dESIGNER<i class="fa" :class="[isShowedDesigner ? up : down]" aria-hidden="true"></i></a></li>
+                <ul class="shop-submenu-list">
+                    <li class="shop-submenu-elements" v-for="item of designer" v-show="isShowedDesigner"><a href="#" class="shop-submenu-link">{{ item }}</a></li>
+                </ul>
             </ul>
         </div>
     `,
     mounted() {
         this.$parent.getJson(`${API+this.menuURL}`)
-            .then(data => {
-                for (let el of data) {
-                    this.menu.push(el);
-                }
-                [this.category, this.brand, this.designer] = [...this.menu];
-                console.log(this.menu)
-                console.log(this.category)
-                console.log(this.brand)
-                console.log(this.designer)
-
+            .then(data => {({categories:this.category, brand:this.brand, designer:this.designer} = data[0]);
             });
     },
 });
